8c422aba875ad03a8184c9fffa76c5136e2ee842
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-29 05:47:27 +0900

    feat(마우스 추적): 추적 요소 생성 & 마우스 이벤트

--- mouseTracking/mouseTracking.js
+++ mouseTracking/mouseTracking.js
@@ -6,14 +6,43 @@ class MouseTracking {
     makeSpace() {
         const container = document.createElement('article');
         container.id = "space";
+        container.style.position = "relative";
         container.style.width = "100%";
         container.style.minHeight = "100vh";
         container.style.backgroundColor = "#333344";
 
         return container;
     }
+    makeTracker() {
+        const tracker = document.createElement('div');
+        tracker.id = "tracker";
+        tracker.style.position = "absolute";
+        tracker.style.width = "50px";
+        tracker.style.height = "50px";
+        tracker.style.backgroundColor = "#165252";
+        tracker.style.borderRadius = "100%";
+
+        return tracker;
+    }
+    setTrackingEvent(space, target) {
+        space.addEventListener('mousemove', (e) => {
+            target.style.top = `${e.clientY - 25}px`;
+            target.style.left = `${e.clientX - 25}px`;
+        });
+        space.addEventListener('click', (e) => {
+            const marker = this.makeTracker();
+            marker.style.top = `${e.clientY - 25}px`;
+            marker.style.left = `${e.clientX - 25}px`;
+            marker.style.backgroundColor = "#898994";
+            space.appendChild(marker);
+        });
+    }
 
     render() {
-        this.root.appendChild(this.makeSpace());
+        const space = this.makeSpace();
+        const tracker = this.makeTracker();
+        this.setTrackingEvent(space, tracker);
+        space.appendChild(tracker);
+        this.root.appendChild(space);
     }
 }
