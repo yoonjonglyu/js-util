807115411dfcbc64061abfe272d247b272255631
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-29 05:20:37 +0900

    feat(마우스 추적): 추적 영역 & RENDER

---
+++ mouseTracking/mouseTracking.js
@@ -0,0 +1,27 @@
+class MouseTracking {
+    constructor(root) {
+        this.root = root;
+    }
+
+    makeSpace() {
+        const container = document.createElement('article');
+        container.id = "space";
+        container.style.width = "100%";
+        container.style.minHeight = "100vh";
+        container.style.backgroundColor = "#333344";
+
+        return container;
+    }
+
+    render() {
+        this.root.appendChild(this.makeSpace());
+    }
+}
+
+
+function Main() {
+    const init = new MouseTracking(document.querySelector('#app'));
+    init.render();
+}
+
+Main();
