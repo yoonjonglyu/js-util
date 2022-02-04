58800779372b2e749955bc36c361e9b18a60529b
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-02-04 19:03:42 +0900

    feat(페이지네이션): 페이지 & 네비게이션 레이아웃

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -1,17 +1,42 @@
 class Pagination {
     constructor(root) {
         this.root = root;
+        this.container = this.makeContainer();
         this.render();
     }
+    makeContainer() {
+        const container = document.createElement('article');
+        container.id = "container";
 
+        return container;
+    }
     makeNav() {
         const nav = document.createElement('nav');
         nav.id = "paginations";
+        const ul = document.createElement('ul');
+        ul.style.listStyle = "none";
+        Array.from({ length: 10 }, (_, idx) => idx + 1)
+            .forEach((el) => {
+                const li = document.createElement('li');
+                li.style.display = "inline-block";
+                li.style.margin = "8px";
+                li.innerText = el;
+                li.addEventListener('click', () => {
+                    this.setPage(el);
+                });
+                ul.appendChild(li);
+            });
+        nav.appendChild(ul);
 
         return nav;
     }
+    setPage(page) {
+        this.container.innerHTML = page;
+    }
     render() {
         const nav = this.makeNav();
+        this.setPage("메인");
+        this.root.appendChild(this.container);
         this.root.appendChild(nav);
     }
 }
