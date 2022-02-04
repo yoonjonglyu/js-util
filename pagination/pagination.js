212874ee1e2ff40d109edea09290da3586982c57
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-02-05 03:05:37 +0900

    feat(페이지네이션): 기본 스타일링

--- pagination/pagination.js
+++ pagination/pagination.js
@@ -8,28 +8,59 @@ class Pagination {
     makeContainer() {
         const container = document.createElement('article');
         container.id = "container";
+        const style = {
+            display: "flex",
+            flexFlow: "column",
+            width: "100%",
+            height: "680px"
+        };
+        for (const [key, value] of Object.entries(style)) {
+            container.style[key] = value;
+        }
 
         return container;
     }
     makeNav(currentPage) {
         const nav = document.createElement('nav');
+        const style = {
+            display: "flex",
+            flexFlow: "column",
+            width: "100%",
+        };
+        for (const [key, value] of Object.entries(style)) {
+            nav.style[key] = value;
+        }
         nav.id = "paginations";
         const ul = document.createElement('ul');
+        const ulStyle = {
+            display: "flex",
+            justifyContent: "center",
+            margin: 0,
+            padding: 0,
+        }
+        for (const [key, value] of Object.entries(ulStyle)) {
+            ul.style[key] = value;
+        }
         ul.style.listStyle = "none";
         Array.from({ length: 10 }, (_, idx) => currentPage * 10 + idx + 1)
             .forEach((el) => {
                 const li = document.createElement('li');
-                li.style.display = "inline-block";
-                li.style.margin = "8px";
+                const liStyle = {
+                    display: "flex",
+                    margin: "8px",
+                    padding: "8px",
+                }
+                for (const [key, value] of Object.entries(liStyle)) {
+                    li.style[key] = value;
+                }
                 li.innerText = el;
                 li.addEventListener('click', () => {
                     this.setPage(el);
                     if (currentPage !== Math.floor(el / 10)) {
                         this.root.removeChild(nav);
-                        const next = this.makeNav(Math.floor(el / 10));
-                        this.root.appendChild(next);
+                        const nextNav = this.makeNav(Math.floor(el / 10));
+                        this.root.appendChild(nextNav);
                     }
-
                 });
                 if (Math.floor(this.items.length / 10) > el) ul.appendChild(li);
             });
@@ -40,10 +71,35 @@ class Pagination {
     setPage(page) {
         this.container.innerText = "";
         const ul = document.createElement('ul');
+        const style = {
+            display: "flex",
+            flexFlow: "column",
+            flex: "1",
+            margin: "8px",
+            padding: 0,
+            listStyle: "none"
+        };
+        for (const [key, value] of Object.entries(style)) {
+            ul.style[key] = value;
+        }
         for (let int = page * 10; int < (page + 1) * 10; int++) {
             const li = document.createElement('li');
             li.className = "page-item";
             li.innerText = this.items[int];
+            const style = {
+                display: "flex",
+                flexFlow: "column",
+                flex: "1",
+                width: "100%",
+                margin: "8px",
+                fontSize: "2rem",
+                color: "red",
+                textAlign: "center",
+                backgroundColor: "#4848e4",
+            };
+            for (const [key, value] of Object.entries(style)) {
+                li.style[key] = value;
+            }
             ul.appendChild(li);
         }
         this.container.appendChild(ul);
