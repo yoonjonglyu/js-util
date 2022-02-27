cdaa40cc6791b89e94906c5fa9682c2637ce6744
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-02-27 17:57:38 +0900

    refactor(프로미스): STATE 구조 변경

--- promiseYou/index.js
+++ promiseYou/index.js
@@ -2,13 +2,16 @@ function Main() {
     const promise = new PromiseYou((resolve, reject) => {
         setTimeout(resolve('testr'), 300);
         setTimeout(reject('testj'), 200);
-    });
+    })
+    .finally(() => console.log('ss'));
+
     const promise1 = promise
         .then((a) => {
             console.log(a);
             return a + 1;
         }, (a) => console.log(a, 2))
-        .catch((b) => console.log(b, "catch"));
+        .catch((b) => console.log(b, "catch"))
+        .finally(() => console.log('aa'));
     const promise2 = promise
         .then((a) => {
             console.log(a);
@@ -20,14 +23,16 @@ function Main() {
         .catch((b) => console.log(b))
         .then((a) => {
             console.log(a);
-        });
+        })
+        .finally(() => console.log('bb'));
 
     const test = new Promise((resolve, reject) => {
-        setTimeout(resolve('testPro2'), 100);
-        setTimeout(reject('testPro'), 500);
-    })
-        .then((a) => console.log(a, 1), (a) => console.log(a, 2))
-        .catch((a) => a)
+        // setTimeout(resolve('testPro2'), 100);
+        // setTimeout(reject('testPro'), 500);
+    });
+    const test1 = new Promise((resolve) => resolve('s'));
+    
+        console.log(test, test1, promise, promise1, promise2);
 }
 
 Main();
--- promiseYou/promiseYou.js
+++ promiseYou/promiseYou.js
@@ -1,37 +1,43 @@
 class PromiseYou {
-    constructor(cb) {
-        this.states = [
-            'pending',
-            'fulfilled',
-            'rejected'
-        ];
-        this.promiseState = this.states[0];
+    constructor(callBack) {
+        this.states = {
+            '<pending>': 'pending',
+            '<fulfilled>': 'fulfilled',
+            '<rejected>': 'rejected'
+        }
+        this.promiseState = this.states['<pending>'];
         this.promiseResult = '';
-        return cb(this.resolve, this.reject);
+        return callBack(this.resolve, this.reject);
     }
     resolve = (value) => {
-        if (this.promiseState !== this.states[0]) return;
-        this.promiseState = this.states[1];
+        if (this.promiseState !== this.states['<pending>']) return;
+        this.promiseState = this.states['<fulfilled>'];
         this.promiseResult = value;
     }
     reject = (value) => {
-        if (this.promiseState !== this.states[0]) return;
-        this.promiseState = this.states[2];
+        if (this.promiseState !== this.states['<pending>']) return;
+        this.promiseState = this.states['<rejected>'];
         this.promiseResult = value;
     }
 
     then(fillBack, rejectBack) {
-        if (this.promiseState === this.states[1]) {
+        if (this.promiseState === this.states['<fulfilled>']) {
             return this.createNewContext(fillBack(this.promiseResult));
         }
-        if (this.promiseState === this.states[2] && rejectBack) {
+        if (this.promiseState === this.states['<rejected>'] && rejectBack) {
             return this.createNewContext(rejectBack(this.promiseResult));
         }
         return this;
     }
     catch(rejectBack) {
-        if (this.promiseState === this.states[2]) {
+        if (this.promiseState === this.states['<rejected>']) {
             return this.createNewContext(rejectBack(this.promiseResult));
+        }
+        return this;
+    }
+    finally(callback) {
+        if (this.promiseState !== this.states['<pending>']) {
+            callback();
         }
         return this;
     }
