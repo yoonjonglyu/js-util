6b61f773e7f9218768fb26b344ccb89bfe558041
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-02 10:39:33 +0900

    feat(노션): 렌더링 & WRAP 컴포넌트

--- rotion/index.js
+++ rotion/index.js
@@ -13,7 +13,27 @@ function Main() {
     loader.require([
         'styledInJs'
     ], (styled) => {
-        console.log(styled);
+
+
+        render(
+            document.querySelector('#app'),
+            Wrap()
+        );
+
+        function render(root, components) {
+            root.appendChild(components);
+        }
+        function Wrap() {
+            const wrap = styled.div`
+                display: flex;
+                flex: 1;
+                width: 100%;
+                height: 100vh;
+                background: #f1f1f1;
+            `;
+            wrap.id = 'wrap';
+            return wrap;
+        }
     });
 }
 
