6decf6a8db63309228228b5622d2456efdd97700
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-02 10:55:09 +0900

    feat(노션): LAYOUT 관련 밑그림

--- rotion/rotion.js
+++ rotion/rotion.js
@@ -1,8 +1,33 @@
 class Rotion {
-    constructor() {
+    constructor(root) {
+        this.root = root;
+        this.container = this.createContainer();
         this.pages = [];
         this.views = {};
         this.loadData();
+    }
+    createContainer() {
+        const container = 'container';
+        this.createArea(container);
+        this.root.appendChild(container);
+        return container;
+    }
+    createArea(container) {
+        const pageList = 'pageList';
+        this.renderPageList(pageList);
+        container.appendChild(pageList);
+
+        const contents = 'contents';
+        container.appendChild(contents);
+    }
+
+    renderPageList(list) {
+        const pages = this.pages.map((item) => {
+            item.idx;
+            item.title;
+        });
+
+        list.appendChild(pages);
     }
 
     loadData() {
