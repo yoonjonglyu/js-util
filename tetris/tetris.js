a85401c22323bea0b4e3e0b310e394cffee4dc9a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-14 22:13:36 +0900

    feat(테트리스): BOARD INIT

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,6 +1,15 @@
 class TetrisState { // 이번에는 여러 클래스로 나누어서 코드를 짜본다.
-    constructor(){
-        this.board = [];
+    constructor(N) {
+        this.board = this.initBoard(N);
+        this.size = N;
+    }
+    initBoard(N) {
+        return Array.from(
+            {
+                length: N
+            },
+            () => new Array(N).fill(0)
+        )
     }
 }
 
