204b56e08d349ec286cfa7a8a4fce74a885778f6
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-14 22:37:35 +0900

    feat(테트리스): BOARD

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -32,4 +32,13 @@ class Tetris {
 
         this.root.appendChild(container);
     }
+    /**
+     * @description 테트리스의 판을 매순간 dom 조작하기에는 리플로우가 너무 많이 일어나므로 
+     * 격자 방식의 board를 만들어 놓고 repaint(각 격자의 color 변경)와 state 조작만을 통해서 게임 진행 사항을 표시해준다.
+     */
+    createBoard() {
+        const Board = document.createElement('table');
+
+        return Board;
+    }
 }
