ae1a9c44817eab7392260a848cbf1dd47f8ed975
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-23 16:47:22 +0900

    feat(스토어): 초기 컨셉

---
+++ store/index.js
@@ -0,0 +1,5 @@
+function Main(){
+    const store = new Store();
+}
+
+Main();
---
+++ store/store.js
@@ -0,0 +1,10 @@
+class Store {
+    constructor(){
+        this._store = {};
+    }
+    /**
+     * 1. 간단히 selector 와 dispatch를 구현하고
+     * 2. 그를 useState훅 처럼 가져다가 쓰는 것을 구현하자.
+     * 3. state를 selector하거나 dispatch하는 경우에 해당 Node를 구독하게 만드는 로직이 필요할 거 같다.
+     */
+}
