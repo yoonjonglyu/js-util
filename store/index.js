8539a987fce5305369f1449948628244848ff207
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-30 00:23:09 +0900

    feat(스토어): 샘플 - 각 컨텍스트 간의 상태공유

--- store/index.js
+++ store/index.js
@@ -1,25 +1,45 @@
 function Main() {
     const store = new Store();
-    const App = document.createElement('main');
-    const Index = document.createElement('div');
-    App.appendChild(Index);
-    const renderIndex = () => {
-        const [Test, setTest] = store.useState({ test: 'a' });
-        const [Test2, setTest2] = store.useState({ test2: 'b' });
-        Index.innerText = `${Test} : ${Test2}`;
-        console.log('렌더링 되었다.');
-        Index.onclick = () => {
-            setTest('sss');
-            setTest2(Math.random());
-        };
+    function App() {
+        const root = document.createElement('main');
+        root.appendChild(Index());
+        root.appendChild(Sub());
+
+        return root;
     }
-    store.watch(renderIndex);
-    const [Test3, setTest3] = store.useState('test2');
-    setTest3('외부에서 호출시 watch 이후에 useState를 호출해야한다.');
-    console.log(store);
+    function Index() {
+        const root = document.createElement('div');
+        root.style.background = 'cyan';
+        const renderIndex = () => {
+            const [Test, setTest] = store.useState({ test: 'a' });
+            const [Test2, setTest2] = store.useState({ test2: 'b' });
+            root.innerText = `${Test} : ${Test2}`;
+            console.log('인덱스가 렌더링 되었다.');
+            root.onclick = () => {
+                setTest('sss');
+                setTest2(Math.random());
+            };
+        };
+        store.watch(renderIndex);
 
+        return root;
+    }
+    function Sub() {
+        const root = document.createElement('div');
+        store.watch(() => {
+            const [Test2, setTest2] = store.useState('test2');
+            const [Test3, setTest3] = store.useState({ test3: 'c' });
+            root.innerText = `${Test3} : ${Test2}`;
+            console.log('서브가 렌더링 되었다.');
+            root.onclick = () => {
+                setTest3('공유 되지 않는 상태 변화');
+            };
+        });
 
-    render(document.querySelector('#app'), App);
+        return root;
+    }
+
+    render(document.querySelector('#app'), App());
 
     function render(root, components) {
         root.appendChild(components);
